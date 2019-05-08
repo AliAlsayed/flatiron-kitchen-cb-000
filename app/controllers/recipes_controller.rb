45493e4011d28fb86240fd38d76d389f36d9b6c4
Ali Alsayed <a_alsayed87@hotmail.com>
2019-05-08 16:30:23 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,6 +12,10 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
   private
 
     def recipe_params
