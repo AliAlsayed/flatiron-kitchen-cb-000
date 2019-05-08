d81f772bbdeaabba3a1f80ac888d88ae940e5b7b
Ali Alsayed <a_alsayed87@hotmail.com>
2019-05-08 15:53:19 +0000

    Automatically backed up by Learn

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,15 @@
+class RecipesController < ApplicationController
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+  end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:name)
+    end
+end
